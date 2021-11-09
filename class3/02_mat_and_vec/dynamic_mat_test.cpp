a89f46da70b9435b3010b33e04d5f390080ee874
spgva <spgva@iscte-iul.pt>
2021-11-09 15:30:27 +0100

    Exercise 1 complete static is faster

--- class3/02_mat_and_vec/dynamic_mat_test.cpp
+++ class3/02_mat_and_vec/dynamic_mat_test.cpp
@@ -5,6 +5,22 @@ using namespace std;
 int main() {
 
   // TODO: fill here the stuff required for exercise 1
+  MatF A(3,2);
+  A.randFill();
 
+  MatF B(2,3);
+  B.randFill();
+
+  MatF C(3,3);
+  C.randFill();
+
+  MatF v(3,1);
+  v.randFill();
+
+  MatF x;
+  for (int i=0; i<1000000; ++i)
+    x = C*(A*B+v*v.transpose());
+
+  cout << x;
 
 }
--- class3/02_mat_and_vec/static_mat_test.cpp
+++ class3/02_mat_and_vec/static_mat_test.cpp
@@ -11,4 +11,22 @@ using Mat3_4f=Mat_<float, 3,4>;
 int main() {
 
   // TODO FILL_HERE_THE_STUFF_FOR_EXERCISE_1
+  Mat_<float,3,2> A;
+  randFill(A);
+
+  Mat_<float,2,3> B;
+  randFill(B);
+
+  Mat_<float,3,3> C;
+  randFill(C);
+
+  Mat_<float,3,1> v;
+  randFill(v);
+
+  Mat_<float,3,3> x;
+  for (int i=0; i<1000000; ++i)
+    x = C*(A*B+v*v.transpose());
+
+  cout << x;
 }
+
