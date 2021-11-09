c2a2f17b76c8cf3707798970d1f34e8aa90eea62
spgva <spgva@iscte-iul.pt>
2021-11-09 14:50:14 +0100

    Init exercise class 3

---
+++ class3/02_mat_and_vec/dynamic_mat_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "mat_f.h"
+using namespace std;
+
+int main() {
+
+  // TODO: fill here the stuff required for exercise 1
+
+
+}
---
+++ class3/02_mat_and_vec/static_mat_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "static_mat.h"
+#include <unistd.h>
+using namespace rp;
+using namespace std;
+
+using Vec3f=Vec_<float, 3>;
+using Mat3f=Mat_<float, 3,3>;
+using Mat3_4f=Mat_<float, 3,4>;
+
+int main() {
+
+  // TODO FILL_HERE_THE_STUFF_FOR_EXERCISE_1
+}
