21084cd0be8e5a5576b2468a09b7b092942be355
spgva <spgva@iscte-iul.pt>
2021-11-09 11:39:44 +0100

    Exercise class 2 init

---
+++ class2/src/btree/btree_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "btree.h"
+
+using namespace std;
+
+int main() {
+  TreeNodeInt* n=new TreeNodeInt(5);
+  n->add(10);
+  n->add(1);
+  n->add(2);
+  n->add(4);
+  cerr << "ok here" << endl;
+  cout << *n << endl;
+}
