f2deecc73b408642e8dc55a8441afac53542c4f4
spgva <spgva@iscte-iul.pt>
2021-11-10 16:35:24 +0100

    first exercise class 5

---
+++ class5/exercise/build/CMakeFiles/int_sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/int_sort.dir/int_sort.cpp.o"
+  "int_sort.pdb"
+  "int_sort"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/int_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ class5/exercise/build/CMakeFiles/point_multisort_gen_data.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/point_multisort_gen_data.dir/point_multisort_gen_data.cpp.o"
+  "point_multisort_gen_data.pdb"
+  "point_multisort_gen_data"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/point_multisort_gen_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
