4664d66e50b31dc96460d932b8b61584db8bb966
spgva <spgva@iscte-iul.pt>
2021-11-12 17:26:04 +0100

    exercise 4 done with ints too

--- class5/src/splitter.h
+++ class5/src/splitter.h
@@ -3,22 +3,21 @@
 template <typename IteratorType_, typename PredicateType_>
 IteratorType_ split(IteratorType_ begin,
                     IteratorType_ end,
-                    const PredicateType_& predicate) {
+                    PredicateType_ predicate) {
   // this represents the type of the element contained
   using ValueType = typename IteratorType_::value_type;
-  // this represents the type of the iterator
-  using IteratorType = IteratorType;
 
   // our game is the following
   // we have an iterator in front, which we move forward
   // let's call it "lower"
-  IteratorType lower=begin;
+  auto lower=begin;
+
   
   // and one iterator on the back, which we move backwards
   // since not all STL does iterators can't be moved backwards
   // we can construct (see documentation)
   // a reverse_iterator, that when incremented moves backwards
-  auto lower=std::make_reverse_iterator(end);
+  auto upper=std::make_reverse_iterator(end);
 
   // reverse and direct iterators cannot be compared straight away,
   // To do so you need to extract a normal iterator
@@ -38,6 +37,7 @@ IteratorType_ split(IteratorType_ begin,
       ++lower;
     } else {
       // todo: anything to write here?
+      std::swap(v_lower,v_upper);
       ++upper; // incrementing a reverse iterator goes backwards
     }
   }
--- class5/src/splitter_test.cpp
+++ class5/src/splitter_test.cpp
@@ -3,61 +3,28 @@
 #include <fstream>
 #include <algorithm>
 #include <cassert>
+#include "splitter.h"
 
 using namespace rp;
 using namespace std;
+//using PointWithIndexList=std::list<PointWithIndex>;
 
-
-// split functions
-// takes two iterators (begin and end)
-// and reorders the data so that the items for which the predicate
-// is true are at the beginning, tho others at the end
-// it returns the iterator to the splitting element between the two classes
-template <typename IteratorType_, typename PredicateType_>
-IteratorType_ split(IteratorType_ begin,
-                    IteratorType_ end,
-                    PredicateType_ predicate) {
-  using ValueType    = typename IteratorType_::value_type;
-  auto lower=begin;  //first
-  // upper is an iterator at the end, it is a reverse iterator,
-  // and moves backward when incremented;
-  // we apply the predicate to each point,
-  // and if the result is positive
-  //   we leave the point where it is
-  // otherwise
-  //   we move the point in the other extrema, and increment the extrema
-  // the iterations end when the upper and lower ends match
-  auto upper=std::make_reverse_iterator(end); 
-
-  // upper.base returns the normal iterator out of the reverse one
-  while (lower!=upper.base()) {
-    ValueType& v_lower=*lower;
-    ValueType& v_upper=*upper;
-    if ( predicate(v_lower) ){
-      ++lower;
-    } else {
-      std::swap(v_lower,v_upper);
-      ++upper;
-    }
-  }
-  return upper.base();
-}
-
-
-using PointWithIndexList=std::list<PointWithIndex>;
-
-struct OddFirst{
-  bool operator()(const PointWithIndex& p){
-    return p.index%2;
+struct isOdd{
+  //bool operator()(const PointWithIndex& p){
+  bool operator()(const int& p){
+    return p%2;
+    //return p.index%2;
   }
 };
 
 int main(int argc, char** argv) {
   assert(argc>1);
   ifstream is(argv[1]);
-  PointWithIndexList points;
+  list<int> points;
+  //PointWithIndexList points;
   while (is) {
-    PointWithIndex p;
+    //PointWithIndex p;
+    int p;
     is >> p;
     if (is) {
       points.push_back(p);
@@ -65,7 +32,7 @@ int main(int argc, char** argv) {
   }
   cerr << "loaded " << points.size()
        << " points from file [" << argv[1] << "]" << endl;
-  split(points.begin(), points.end(), OddFirst());
+  split(points.begin(), points.end(), isOdd());
 
   for(const auto& p: points)
     cerr << p << endl;
